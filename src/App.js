6d0aa872152ffc2cd0141cfbe86f646bb49522fb
Adegboyega Adedamola Wilson <wildam001@gmail.com>
2024-01-26 10:27:05 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Dashboard from './components/Dashboard';
+
+function App() {
+  return (
+    <div className="max-w-[2500px]">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
